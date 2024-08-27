a12848102b7d24082f5a5e085c9997bce3f5896e
domser2002 <domser2002@o2.pl>
2024-08-27 11:33:26 +0200

    struct fields

--- graph.h
+++ graph.h
@@ -1,7 +1,8 @@
 typedef struct Graph
 {
-
+    int vertexCount;
+    int **edges; // matrix of neighbourship
 };
 
-Graph* construct_graph();
+Graph* construct_graph(int vertexCount);
 void destruct_graph(Graph* g);

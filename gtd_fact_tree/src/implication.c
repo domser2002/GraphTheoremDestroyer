2042e96b3b30e25216f9f59da71b062fcb0164da
domser2002 <domser2002@o2.pl>
2024-10-12 22:24:33 +0200

    introduce implication sides

--- gtd_fact_tree/src/implication.c
+++ gtd_fact_tree/src/implication.c
@@ -1,4 +1,42 @@
 #include "implication.h"
+#define KNOWN_IMPLICATIONS_NUMBER 2
+
+typedef bool (*implication_occurs_fun) (int *, int *); 
+
+typedef struct Implication_side {
+    bool types[FACT_TYPE_NUM];
+    int n_facts;
+    int n_params;
+    int type_to_param_idx[FACT_TYPE_NUM][MAX_PARAMS_IN_FACT];
+} Implication_side;
+typedef struct Implication {
+    implication_occurs_fun occurs;
+    Implication_side left_side;
+    Implication_side right_side;
+} Implication;
+
+bool implication_type_1_occurs(int *left_side_params, int *right_side_params)
+{
+    GTD_UNUSED(left_side_params);
+    GTD_UNUSED(right_side_params);
+    return false;
+}
+
+bool implication_type_2_occurs(int *left_side_params, int *right_side_params)
+{
+    GTD_UNUSED(left_side_params);
+    GTD_UNUSED(right_side_params);
+    return false;
+}
+
+Implication knownImplicationsArray[KNOWN_IMPLICATIONS_NUMBER] = {
+    {
+
+    },
+    {
+
+    }
+};
 
 Fact *implies(Fact *fact)
 {

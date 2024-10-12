ba705e1f3db26fa688f3a08f04d408932fe73d6e
domser2002 <domser2002@o2.pl>
2024-10-12 18:17:26 +0200

    refactor tests

--- gtd_fact_tree/src/test_fact.c
+++ gtd_fact_tree/src/test_fact.c
@@ -3,24 +3,33 @@
 typedef Fact *(*create_fact_func)(int);
 typedef int (*delete_fact_func)(Fact *);
 
-static void single_contradict_test(create_fact_func create_fact1, int fact1_param, create_fact_func create_fact2, int fact2_param, delete_fact_func delete_fact1, delete_fact_func delete_fact2, bool expected)
+/**
+ * \brief helper function to perform a single test on a contradict method
+ * \param create_fact1 pointer to function used to create first fact
+ * 
+*/
+static void single_contradict_test(int n_facts, create_fact_func *create_facts, delete_fact_func *delete_facts, int *fact_params, bool expected)
 {
     // Arrange
-    Fact **factArray = (Fact **)gtd_malloc(2*sizeof(Fact*));
-    factArray[0] = create_fact1(fact1_param);
-    factArray[1] = create_fact2(fact2_param);
+    Fact **factArray = (Fact **)gtd_malloc(n_facts*sizeof(Fact*));
+    for(int i=0; i<n_facts; i++)
+    {
+        factArray[i] = create_facts[i](fact_params[i]);
+    }
     // Act
-    bool result = contradict(factArray,2);
+    bool result = contradict(factArray,n_facts);
     // Clean
-    delete_fact1(factArray[0]);
-    delete_fact2(factArray[1]);
+    for(int i=0; i<n_facts; i++)
+    {
+        delete_facts[i](factArray[i]);
+    }
     gtd_free(factArray);
     // Assert
     assert(result == expected);
 }
 
 /**
-* \brief function to check if contradiction correctly recognizes contradiction type 1 - 
+* \brief function to check if contradict correctly recognizes contradiction type 1 - 
 contradiction between max vertex count and min edge count
 * \param maxVertexCount max vertex count
 * \param minEdgeCount min edge count
@@ -28,25 +37,75 @@ contradiction between max vertex count and min edge count
 */
 static void test_contradiction_1(int maxVertexCount, int minEdgeCount, bool expected)
 {
-    single_contradict_test(&create_max_vertex_count_fact, maxVertexCount, 
-    &create_min_edge_count_fact, minEdgeCount,
-    &delete_max_vertex_count_fact, &delete_min_edge_count_fact, expected);
+    int n_facts = 2;
+    create_fact_func *create_fact_functions = (create_fact_func*)gtd_malloc(n_facts*sizeof(create_fact_func));
+    delete_fact_func *delete_fact_functions = (delete_fact_func*)gtd_malloc(n_facts*sizeof(delete_fact_func));
+    int *fact_params = (int*)gtd_malloc(n_facts*sizeof(int));
+    create_fact_functions[0] = &create_max_vertex_count_fact;
+    delete_fact_functions[0] = &delete_max_vertex_count_fact;
+    fact_params[0] = maxVertexCount;
+    create_fact_functions[1] = &create_min_edge_count_fact;
+    delete_fact_functions[1] = &delete_min_edge_count_fact;
+    fact_params[1] = minEdgeCount;
+    single_contradict_test(n_facts,create_fact_functions,delete_fact_functions,fact_params,expected);
+    gtd_free(create_fact_functions);
+    gtd_free(delete_fact_functions);
+    gtd_free(fact_params);
 }
 
+/**
+* \brief function to check if contradict correctly recognizes contradiction type 2 - 
+contradiction between min vertex count and max vertex count
+* \param minVertexCount min vertex count
+* \param maxVertexCount max vertex count
+* \param expected expected test result, true if facts contradict
+*/
 static void test_contradiction_2(int minVertexCount, int maxVertexCount, bool expected)
 {
-    single_contradict_test(&create_min_vertex_count_fact, minVertexCount, 
-    &create_max_vertex_count_fact, maxVertexCount,
-    &delete_min_vertex_count_fact, &delete_max_vertex_count_fact, expected);
+    int n_facts = 2;
+    create_fact_func *create_fact_functions = (create_fact_func*)gtd_malloc(n_facts*sizeof(create_fact_func));
+    delete_fact_func *delete_fact_functions = (delete_fact_func*)gtd_malloc(n_facts*sizeof(delete_fact_func));
+    int *fact_params = (int*)gtd_malloc(n_facts*sizeof(int));
+    create_fact_functions[0] = &create_min_vertex_count_fact;
+    delete_fact_functions[0] = &delete_min_vertex_count_fact;
+    fact_params[0] = minVertexCount;
+    create_fact_functions[1] = &create_max_vertex_count_fact;
+    delete_fact_functions[1] = &delete_max_vertex_count_fact;
+    fact_params[1] = maxVertexCount;
+    single_contradict_test(n_facts,create_fact_functions,delete_fact_functions,fact_params,expected);
+    gtd_free(create_fact_functions);
+    gtd_free(delete_fact_functions);
+    gtd_free(fact_params);
 }
 
+/**
+* \brief function to check if contradict correctly recognizes contradiction type 3 - 
+contradiction between min edge count and max edge count
+* \param minEdgeCount min edge count
+* \param maxEdgeCount max edge count
+* \param expected expected test result, true if facts contradict
+*/
 static void test_contradiction_3(int minEdgeCount, int maxEdgeCount, bool expected)
 {
-    single_contradict_test(&create_min_edge_count_fact, minEdgeCount, 
-    &create_max_edge_count_fact, maxEdgeCount,
-    &delete_min_vertex_count_fact, &delete_max_vertex_count_fact, expected);
+    int n_facts = 2;
+    create_fact_func *create_fact_functions = (create_fact_func*)gtd_malloc(n_facts*sizeof(create_fact_func));
+    delete_fact_func *delete_fact_functions = (delete_fact_func*)gtd_malloc(n_facts*sizeof(delete_fact_func));
+    int *fact_params = (int*)gtd_malloc(n_facts*sizeof(int));
+    create_fact_functions[0] = &create_min_edge_count_fact;
+    delete_fact_functions[0] = &delete_min_edge_count_fact;
+    fact_params[0] = minEdgeCount;
+    create_fact_functions[1] = &create_max_edge_count_fact;
+    delete_fact_functions[1] = &delete_max_edge_count_fact;
+    fact_params[1] = maxEdgeCount;
+    single_contradict_test(n_facts,create_fact_functions,delete_fact_functions,fact_params,expected);
+    gtd_free(create_fact_functions);
+    gtd_free(delete_fact_functions);
+    gtd_free(fact_params);
 }
 
+/**
+ * \brief function to run all tests for the contradict method
+*/
 static void test_contradict(void)
 {
     test_contradiction_1(1,1,true);

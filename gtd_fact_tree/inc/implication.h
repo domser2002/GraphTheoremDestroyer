52120946ddac6cb9437d8fe11b7e8b201dcae8fb
domser2002 <domser2002@o2.pl>
2024-10-13 18:15:56 +0200

    known implications defined

--- gtd_fact_tree/inc/implication.h
+++ gtd_fact_tree/inc/implication.h
@@ -2,5 +2,5 @@
 #define IMPLICATION_H
 #define CAN_ACCESS_FACT
 #include "fact.h"
-Fact **implies(Fact **factArray, int *count);
+Fact **implies(Fact **factArray, int n_facts, int *count);
 #endif
--- gtd_fact_tree/src/implication.c
+++ gtd_fact_tree/src/implication.c
@@ -1,28 +1,28 @@
 #include "implication.h"
 #define KNOWN_IMPLICATIONS_NUMBER 2
 
-typedef bool (*implication_occurs_fun) (int *, int *); 
+typedef bool (*calc_right_side_params_fun) (int *, int *); 
 
-typedef struct Implication_side {
+typedef struct Implication_Side {
     bool types[FACT_TYPE_NUM];
     int n_facts;
     int n_params;
     int type_to_param_idx[FACT_TYPE_NUM][MAX_PARAMS_IN_FACT];
-} Implication_side;
+} Implication_Side;
 typedef struct Implication {
-    implication_occurs_fun occurs;
-    Implication_side left_side;
-    Implication_side right_side;
+    calc_right_side_params_fun calculate_params;
+    Implication_Side left_side;
+    Implication_Side right_side;
 } Implication;
 
-bool implication_type_1_occurs(int *left_side_params, int *right_side_params)
+bool implication_type_1_calculate_right_side_params(int *left_side_params, int *right_side_params)
 {
     GTD_UNUSED(left_side_params);
     GTD_UNUSED(right_side_params);
     return false;
 }
 
-bool implication_type_2_occurs(int *left_side_params, int *right_side_params)
+bool implication_type_2_calculate_right_side_params(int *left_side_params, int *right_side_params)
 {
     GTD_UNUSED(left_side_params);
     GTD_UNUSED(right_side_params);
@@ -31,14 +31,46 @@ bool implication_type_2_occurs(int *left_side_params, int *right_side_params)
 
 Implication knownImplicationsArray[KNOWN_IMPLICATIONS_NUMBER] = {
     {
-
+        .left_side = {
+            .types = {false, false, true, false},
+            .n_facts = 1,
+            .n_params = 1,
+            .type_to_param_idx = {{-1},{-1},{0},{-1}}
+        },
+        .right_side = {
+            .types = {false, true, false, false},
+            .n_facts = 1,
+            .n_params = 1,
+            .type_to_param_idx = {{-1},{0},{-1},{-1}}            
+        },
+        .calculate_params = &implication_type_1_calculate_right_side_params
     },
     {
-
+        .left_side = {
+            .types = {false, true, false, false},
+            .n_facts = 1,
+            .n_params = 1,
+            .type_to_param_idx = {{-1},{0},{-1},{-1}}
+        },
+        .right_side = {
+            .types = {false, false, false, true},
+            .n_facts = 1,
+            .n_params = 1,
+            .type_to_param_idx = {{-1},{-1},{-1},{0}}            
+        },
+        .calculate_params = &implication_type_2_calculate_right_side_params
     }
 };
 
-Fact **implies(Fact **factArray, int *count)
+/**
+ * \brief function that checks if array of facts is the left side of a known implication
+ *  and returns right side of it in that case
+ * \param factArray array of facts (potential left side)
+ * \param n_facts number of elements in factArray
+ * \param count pointer to return number of elements on the right side
+ * \return right side of implication or NULL if there is no implication with that left side defined
+*/
+Fact **implies(Fact **factArray, int n_facts, int *count)
 {
     GTD_UNUSED(factArray);
     count = 0;

3da6673d5687b36a58fdb603dba381d0be317e17
Dominik Seredyn <86359641+domser2002@users.noreply.github.com>
2024-10-28 11:41:05 +0100

    [FactTree] Optimize implementation of FactTree class considering graph structure (#19)

---
+++ gtd_fact_tree/inc/fact_tree.h
@@ -0,0 +1,18 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <stdint.h>
+#include <stdbool.h>
+#include "fact.h"
+
+typedef struct FactTree
+{
+    uint32_t fact_count;
+    uint32_t **parents; 
+    uint8_t *parent_count;
+    Fact **facts;
+} FactTree;
+
+FactTree *construct(uint32_t fact_count, Fact **facts);
+void destruct(FactTree* g);
+bool add_fact(FactTree *g, uint32_t *parent_idxs, uint8_t parent_count, Fact *new_fact);
+#endif

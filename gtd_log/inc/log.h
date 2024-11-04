720f99fedaaf27b1d7e3da35a37782bffba365c8
Dominik Seredyn <86359641+domser2002@users.noreply.github.com>
2024-11-04 21:50:55 +0100

    [LOG] Reorganize and optimize logs structure (#20)

--- gtd_log/inc/log.h
+++ gtd_log/inc/log.h
@@ -2,8 +2,11 @@
 #define LOG_H
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define GTD_LOG(...) write_log_to_file("log.txt",__VA_ARGS__)
+extern bool test_mode;
 
 void write_log_to_stdout(const char *format, ...);
 void write_log_to_file(const char *filepath, const char *format, ...);
